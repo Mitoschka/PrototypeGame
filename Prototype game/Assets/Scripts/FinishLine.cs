b3a827b4c5dc1a1fab6f7b20c4545cc5cbe40dd9
Michael-200 <55751246+Michael-200@users.noreply.github.com>
2020-05-14 22:29:45 +0600

    Add finish line

---
+++ Prototype game/Assets/Scripts/FinishLine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinishLine : MonoBehaviour
+{
+    public Text winText;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    //Turn on if object is triggerit
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "Vehicle")
+        {
+            winText.text = "You Win!";
+
+            StartCoroutine(Return());
+        }
+        //Wait for seconds real time
+        IEnumerator Return()
+        {
+            yield return new WaitForSecondsRealtime(1.7f);
+            winText.text = "";
+        }
+
+    }
+}
